9edd9c874e0ca31d8007631353bf9f3bfca7eefb
Hunter Loftis <hunter@hunterloftis.com>
2019-01-14 13:43:12 -0500

    better memory system (Buffer)

--- server.js
+++ server.js
@@ -1,51 +1,49 @@
-const throng = require('throng');
+const throng = require('throng')
 
-const WORKERS = process.env.WEB_CONCURRENCY || 1;
-const PORT = process.env.PORT || 3000;
+const WORKERS = process.env.WEB_CONCURRENCY || 1
+const PORT = process.env.PORT || 3000
 
 throng({
   workers: WORKERS,
   lifetime: Infinity
-}, start);
+}, start)
 
 function start() {
-  const crypto = require('crypto');
-  const express = require('express');
-  const app = express();
+  const crypto = require('crypto')
+  const express = require('express')
+  const app = express()
 
   app
     .get('/cpu', cpuBound)
     .get('/memory', memoryBound)
     .get('/io', ioBound)
     .get('/', hello)
-    .listen(PORT, onListen);
+    .listen(PORT, onListen)
 
   function hello(req, res, next) {
     res.send('Hello, world')
   }
 
   function cpuBound(req, res, next) {
-    const key = Math.random() < 0.5 ? 'ninjaturtles' : 'powerrangers';
-    const hmac = crypto.createHmac('sha512WithRSAEncryption', key);
-    const date = Date.now() + '';
-    hmac.setEncoding('base64');
-    hmac.end(date, () => res.send('A hashed date for you! ' + hmac.read()));
+    const key = Math.random() < 0.5 ? 'ninjaturtles' : 'powerrangers'
+    const hmac = crypto.createHmac('sha512WithRSAEncryption', key)
+    const date = Date.now() + ''
+    hmac.setEncoding('base64')
+    hmac.end(date, () => res.send('A hashed date for you! ' + hmac.read()))
   }
 
   function memoryBound(req, res, next) {
-    const meg = new Array(1024 * 1024).join('X');
-    setTimeout(function sendResponse() {
-      res.send('Large response: ' + meg);
-    }, 20).unref();
+    const meg = Buffer.alloc(1024 * 1024, 'X')
+    res.send('Allocated 1 MB buffer')
   }
 
   function ioBound(req, res, next) {
     setTimeout(function SimulateDb() {
-      res.send('Got response from fake db!');
-    }, 300).unref();
+      res.send('Got response from fake db!')
+    }, 300).unref()
   }
 
   function onListen() {
-    console.log('Listening on', PORT);
+    console.log('Listening on', PORT)
   }
 }
